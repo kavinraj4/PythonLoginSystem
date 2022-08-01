d0be6a11c0fd79206b0c09c810bc387b1438eee2
Kavin <106822632+kavinraj4@users.noreply.github.com>
2022-08-01 16:58:14 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+
+setup(name='PythonLoginSystem',
+      version='0.1',
+      description='Basic Login and Registration in Python',
+      long_description=long_desc,
+      url='https://github.com/kavinraj4/Python-Login-System',
+      author='kavinraj4',
+      author_email='kavinraj2200@gmail.com',
+      license='MIT',
+      packages=['src'],
+      zip_safe=False)
